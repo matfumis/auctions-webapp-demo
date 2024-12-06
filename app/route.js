f6f3749f84d5def3f7b668c3d6751e807a614179
Matteo Fumis <matfumis@gmail.com>
2024-12-06 15:39:53 +0100

    Implemented other routes

--- app/route.js
+++ app/route.js
@@ -4,22 +4,6 @@ const db = require("./db.js");
 const jwt = require("jsonwebtoken");
 const secret = 'secret';
 
-/*
-const verifyToken = (req, res, next) => {
-  const authHeader = req.headers['cookie'];
-  const token = authHeader && authHeader.split(' ')[1];
-
-  if (!token) return res.status(401).send('Token required');
-
-  jwt.verify(token, secret, (err, user) => {
-    if (err) return res.status(403).send('Invalid or expired token');
-    req.username = user;
-    next();
-  });
-};
-
- */
-
 const verifyToken = (req, res, next) => {
   const token = req.cookies.token;
   if (!token) {
@@ -96,12 +80,54 @@ router.get('/bids/:id', async (req, res) => {
 });
 
 router.get('/whoami', verifyToken, async (req, res) => {
-  const {id} = jwt.decode(req.cookies.token);
   const mongo = await db.connectToDb();
+  const {id} = jwt.decode(req.cookies.token);
   const user = await mongo.collection('users').findOne({id: parseInt(id)});
   res.json(user);
 })
 
+router.post('/auctions', verifyToken, async (req, res) => {
+  const mongo = await db.connectToDb();
+  const {id} = jwt.decode(req.cookies.token);
+  const auction = {
+    id: generateId(),
+    title: req.body.title,
+    description: req.body.description,
+    sellerId: id,
+    startPrice: parseInt(req.body.startPrice),
+    currentPrice: parseInt(req.body.startPrice),
+    startTime: new Date(), //per il momento le aste sono create istantaneamente
+    endTime: req.body.endTime,
+    status: 'open',
+    bids: []
+  }
 
+  await mongo.collection('auctions').insertOne(auction);
+  res.json(auction);
+
+});
+
+router.put('/auctions/:id', verifyToken, async (req, res, err) => {
+  const mongo = await db.connectToDb();
+  const auction = await mongo.collection('auctions').findOne({id: parseInt(req.params.id)});
+  const {id} = jwt.decode(req.cookies.token);
+  const requestingUserId = parseInt(id);
+  const auctionSellerId = parseInt(auction.sellerId);
+  if (requestingUserId === auctionSellerId) {
+    let changes = {
+      title: req.body.title,
+      description: req.body.description,
+      endTime: req.body.endTime,
+    }
+    await mongo.collection('auctions').updateOne(changes, auction);
+    res.json(auction);
+  }
+  else{
+    res.status(400).send(err);
+  }
+});
+
+
+const generateId = () => Math.floor(10000 + Math.random() * 90000);
 
 module.exports = router;
