990777699752d77cf97c0be334713ff2ddaf6445
matfumis <matfumis@gmail.com>
2024-12-29 20:47:38 +0100

    Added db population

--- app/app.js
+++ app/app.js
@@ -3,8 +3,11 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const router = require("./route.js");
 const auth = require('./auth.js');
+const populate = require("./utils/populateDb.js");
 
 const port = 8000;
+
+
 
 app.use(cookieParser());
 app.use(express.json());
@@ -14,6 +17,12 @@ app.use(express.static("public"));
 app.use('/api/auth', auth);
 app.use('/api', router);
 
-app.listen(port, () => {
+app.listen(port, async () => {
+  try {
+    await populate.seedData();
+    console.log("Database initialized");
+  } catch (error) {
+    console.error("Error initializing database");
+  }
   console.log(`Web server started on port ${port}`);
 });
